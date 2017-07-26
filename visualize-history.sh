63601199b5763a9f48dddb1a7f82c4fc79e3f561
Jan Warchoł <jan.warchol@gmail.com>
2017-07-26 08:37:16 +0200

    Allow multiple concurrent log views (for multiple repos)

--- visualize-history.sh
+++ visualize-history.sh
@@ -2,6 +2,8 @@
 # `watch` command doesn't work with colored git log - use shell loop
 
 COMMAND='git log --oneline --decorate --graph --color --branches --remotes'
+OUTPUT="/tmp/git-log-output`pwd`"
+mkdir -p `dirname "$OUTPUT"`
 
 while true; do
   # trim command output in case it doesn't fit the screen
@@ -10,12 +12,12 @@ while true; do
   OUTPUT_HEIGHT=$(echo $TERMINAL_HEIGHT - 3 | bc)
 
   # use a temp file to avoid blinking
-  $COMMAND | head -${OUTPUT_HEIGHT} > /tmp/git-log-output
+  $COMMAND | head -${OUTPUT_HEIGHT} > "$OUTPUT"
 
   # clear terminal buffer completely (as opposed to "clear" command)
   tput reset
   printf "[%s] History visualization:\n\n" "$(date)"
   # less allows disabling line wrapping (as opposed to "cat")
-  less -SXE /tmp/git-log-output
+  less -SXE "$OUTPUT"
   sleep 0.2
 done
